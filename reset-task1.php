f2af7805b83d55ec38ff56b966e482dd1b8235e4
Anh Pham <duyanhpham1509@gmail.com>
2017-05-09 09:13:14 +0300

    refactor: update reset db task 1.

--- reset-task1.php
+++ reset-task1.php
@@ -1,6 +1,6 @@
 <?php
 	include "database.php";
-	
+
 	$database = getConn();
     $sql = "DELETE FROM users";
     $result = $database->query($sql);
@@ -8,7 +8,20 @@
     $sql = "DELETE FROM tasks";
     $result = $database->query($sql);
 
-    echo "Database reset ok!";
+    $sql = "INSERT INTO users (email, username, password) VALUES ('john@example.com', 'John', 'Dodo');";
+    $sql .= "INSERT INTO users (email, username, password) VALUES ('mary@example.com', 'Mary', 'Mary96');";
+    $sql .= "INSERT INTO users (email, username, password) VALUES ('matteo@example.com', 'matteo', 'pippo');";
+    $sql .= "INSERT INTO users (email, username, password) VALUES ('jusy@example.com', 'giu', 'giugiu21');";
+    $sql .= "INSERT INTO users (email, username, password) VALUES ('vale87@example.com', 'vales', 'ValesVales');";
+    $sql .= "INSERT INTO users (email, username, password) VALUES ('nicola@example.com', 'nik', 'c834cola');";
+    $sql .= "INSERT INTO users (email, username, password) VALUES ('pino@example.com', 'pino', 'three32');";
+    $result = $database->query($sql);
+
+    if ($database->multi_query($sql) === TRUE) {
+    	echo "Database reset ok!";
+    } else {
+    	echo "Error: " . $sql . "<br>" . $database->error;
+    }
 
     $database->close();
 ?>
